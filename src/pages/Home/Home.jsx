d6dbd28100168b60b7c19dd498e9a517f95fe871
SubhaNITS150 <156669469+SubhaNITS150@users.noreply.github.com>
2024-07-09 00:20:48 +0530

    Collaboration Section (#2)
    * Added Collaboration
    
    * Added Collaboration Section
    
    * Added Collaboration and What is NITSMUN page
    
    ---------
    
    Co-authored-by: SubhaNITS150 <subhajyoti_ug_23@cse.nits.ac.in>

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import styles from "./Home.module.scss";
 import * as components from "../../components/index";
+import Collaboration from "../../components/Collaboration/Collaboration";
+import What from "../../components/What/What";
+
 const Home = () => {
   return (
     <div className={styles.parent}>
       <components.Navbar />
       Home
+      <Collaboration />
+      <What />
     </div>
   );
 };
