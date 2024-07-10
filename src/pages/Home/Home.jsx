182977a8f8aef62881df78a4a2af06ad7c324c9d
Dhananjoy Das <dasdhananjoy733@gmail.com>
2024-07-11 00:37:53 +0530

    Hero and Navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import styles from "./Home.module.scss";
 import * as components from "../../components/index";
 import Collaboration from "../../components/Collaboration/Collaboration";
 import What from "../../components/What/What";
+import Hero from "../../components/Hero/Hero";
 
 const Home = () => {
   return (
     <div className={styles.parent}>
       <components.Navbar />
       Home
+      <Hero />
       <components.Testimonial />
       <Collaboration />
       <What />
