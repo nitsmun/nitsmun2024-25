89d5564ec9c838cd72bc833e02e546f9e1ecf615
Barnil Sarma <72012568+barnilsarma@users.noreply.github.com>
2024-10-01 15:17:53 +0530

    few bug fixes (#24)

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -2,26 +2,36 @@
 import styles from "./Events.module.scss";
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Events = () => {
   return (
     <div className={styles.events}>
       <h1 className={styles.tittle}>OUR EVENTS</h1>
-
-      <div className={styles.youth}>
-        <div className={styles.pic}>
-          <h2>YOUTH PARLIAMENT</h2>
+      <div className={styles.innerCont}>
+        <div className={styles.firstRow}>
+          <Link to="/Events/Youth">
+            <div className={styles.youth}>
+              <div className={styles.pic}>
+                <h2>YOUTH PARLIAMENT</h2>
+              </div>
+            </div>
+          </Link>
+          <Link to="/Events/Mockmun">
+            <div className={styles.mock}>
+              <div className={styles.pic}>
+                <h2>MOCK MUN</h2>
+              </div>
+            </div>
+          </Link>
         </div>
-      </div>
-      <div className={styles.mock}>
-        <div className={styles.pic}>
-          <h2>MOCK MUN</h2>
-        </div>
-      </div>
-      <div className={styles.annual}>
-        <div className={styles.pic}>
-          <h2>ANNUAL CONFERENCE</h2>
-        </div>
+        <Link to="/Events/Annual">
+          <div className={styles.annual}>
+            <div className={styles.pic}>
+              <h2>ANNUAL CONFERENCE</h2>
+            </div>
+          </div>
+        </Link>
       </div>
     </div>
   );
