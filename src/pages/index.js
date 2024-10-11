13deb41a399d52e62460c96f895d8630f787920b
vyyvek <beingviveksharma99@gmail.com>
2024-10-11 19:22:38 +0530

    4th commit testimonial fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,11 @@ import Events from "./Events/Events";
 import Youth from "./Youth/Youth";
 import Mockmun from "./Mockmun/Mockmun";
 import Annual from "./Annual/First";
-<<<<<<< HEAD
 
-=======
->>>>>>> d091128ee99d69351cc9b46017b46970466cc989
+
 import Contact from "../components/Contact/Contact";
 import FaqSection from "./Faqs/FaqSection";
 
 export { Home, Team, Events, Youth, Mockmun, Annual, Contact, FaqSection };
 
-<<<<<<< HEAD
 
-
-=======
->>>>>>> d091128ee99d69351cc9b46017b46970466cc989
