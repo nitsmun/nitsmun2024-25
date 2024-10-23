6381ff4c2f147722af04f212773ce1a7d0a898ae
barnilsarma <barnilsarma@gmail.com>
2024-10-23 16:00:20 +0530

    merge conflict resolve

--- src/pages/Team/Card.jsx
+++ src/pages/Team/Card.jsx
@@ -29,27 +29,15 @@ const Card = (props) => {
           )}
           {props.mail != "" && (
             <a href={props.mail} target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-mail"></i>
-            </a>
-          )}
-          {props.fb && (
-            <a href={props.fb} target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-facebook"></i>
-            </a>
-          )}
-          {props.insta != "" && (
-            <a href={props.insta} target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-instagram"></i>
-            </a>
-          )}
-          {props.linkedin != "" && (
-            <a href={props.linkedin} target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-linkedin"></i>
-            </a>
-          )}
-          {props.mail != "" && (
-            <a href={props.mail} target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-mail"></i>
+              <img
+                style={{
+                  height: "1.5rem",
+                  width: "1.5rem",
+                  filter: "invert(100%)",
+                }}
+                alt="Mail icon Loading..."
+                src="https://res.cloudinary.com/dhry5xscm/image/upload/v1699636486/nitsmun/envelope-regular_sbnoql.svg"
+              />
             </a>
           )}
         </div>
