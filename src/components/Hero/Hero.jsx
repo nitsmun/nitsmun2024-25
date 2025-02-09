7215145b77084f58fbe8d77b0889c2e35faece9c
barnilsarma <barnilsarma@gmail.com>
2025-02-09 15:52:34 +0530

    login bug fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,15 @@
 import styles from "./Hero.module.scss";
 import { auth, provider } from "../../config";
 import { signInWithPopup } from "firebase/auth";
+import { useEffect,useState } from "react";
 const Hero = () => {
+  const [signedin,setSignedin]=useState(false);
   const handleClick = () => {
-    console.log("ok");
     signInWithPopup(auth, provider).then((data) => {
       if (data.user && data.user.email) {
         const userEmail = data.user.email.toLowerCase();
         localStorage.setItem("email", userEmail);
+        setSignedin(true);
       } else {
         console.error("Error retrieving user data during sign-in.");
       }
@@ -15,13 +17,17 @@ const Hero = () => {
   };
   const handleLogout = () => {
     localStorage.removeItem("email");
+    setSignedin(false);
   };
+  useEffect(()=>{
+    setSignedin(signedin);
+  },[signedin]);
   const logoUrl =
     "https://res.cloudinary.com/dybbffhed/image/upload/v1720263284/qjwddcaqsaptighoydzx.png";
   return (
     <div className={styles.hero}>
       <div className={styles.loginCont}>
-        {!localStorage.getItem("email") ? (
+        {!localStorage.getItem("email") && !signedin ? (
           <button onClick={handleClick}>LOGIN</button>
         ) : (
           <button onClick={handleLogout}>LOGOUT</button>
